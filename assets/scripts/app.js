fa3833b5d0776c576567f3b04ef7abf1d52c4bc9
Jam <jamhejratipro@gmail.com>
2022-03-18 13:40:01 +0330

    sum function added

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,7 +1,19 @@
-numberBtn.forEach((number) => {
-   number.addEventListener("click", () => {
-    let currentNum = "";
-    currentNum += number.innerHTML;
-    resultViewer.innerHTML += currentNum;
-   });
-});
+let currentInputNum = '';
+let currentValue = 0;
+
+const numberHandler = (btnValue) =>{
+    currentInputNum += btnValue;
+    resultViewer.innerHTML = currentInputNum;
+    console.log(currentInputNum);
+}
+
+const sumHandler = () =>{
+    currentValue += Number(currentInputNum);
+    currentInputNum = '';
+    resultViewer.innerHTML = currentValue;
+}
+
+numberBtn.forEach(number => {
+    number.addEventListener('click' , numberHandler.bind(this , number.innerHTML));
+})
+sumBtn.addEventListener('click' , sumHandler);
